3b1a6ae134756668a38a62f9cfde9e7b323ba6af
ashraf <ashrafabusunaina1986>
2023-08-22 14:57:35 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href={'/menu'}>Menu</Link>
+      <Link  className={styles.link} href={'/menu'}>Menu</Link>
     </main>
   )
 }
