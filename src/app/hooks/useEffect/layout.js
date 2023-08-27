9a73d82567e3eb0c45c8f53a7546fa59d1e3137b
ashraf <ashrafabusunaina1986>
2023-08-27 16:52:50 +0300

    first commit

--- src/app/hooks/useEffect/layout.js
+++ src/app/hooks/useEffect/layout.js
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react'
+import React, {useEffect, useState } from 'react'
 import Header from '@/components/hooks/use_effect/header/Header'
 import styles from './layout.module.css'
 import UseEffect from './page'
@@ -8,9 +8,13 @@ function Layout(props) {
 
   const [isActive,setIsActive]=useState(false)
   const activeHandler=()=>{
+    localStorage.setItem('isLogged','0')
     setIsActive(false)
   }
-  
+  useEffect(()=>{
+    const isLogged=localStorage.getItem('isLogged')
+    isLogged==='1'?setIsActive(true):setIsActive(false)
+  })
   return (
     <div className={styles.layout}>
         <Header auth={isActive} activeHandler={activeHandler}/>
--- src/components/hooks/use_effect/login/Login.js
+++ src/components/hooks/use_effect/login/Login.js
@@ -1,5 +1,5 @@
 'use client'
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from './login.module.css'
 import Button from '../../../Button/Button'
 
@@ -13,12 +13,19 @@ function Login(props) {
     const email=emailRef.current.value
     const pass=passRef.current.value
     if(email.trim().length===0 || pass.trim().length===0){
+      localStorage.setItem('isLogged','0')
       props.setIsActive(false)
     }else{
+      localStorage.setItem('isLogged','1')
       props.setIsActive(true)
     }
     
   }
+
+  useEffect(()=>{
+    const isLogged=localStorage.getItem('isLogged')
+    isLogged==='1'?props.setIsActive(true):props.setIsActive(false)
+  })
   
   return (
     <form className={styles.form}>
