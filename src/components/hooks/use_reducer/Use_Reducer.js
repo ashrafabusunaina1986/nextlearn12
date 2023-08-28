3c1ad38587a450494127e21aba40f70ab8ef5769
ashraf <ashrafabusunaina1986>
2023-08-28 20:15:19 +0300

    first commit

--- src/components/hooks/use_reducer/Use_Reducer.js
+++ src/components/hooks/use_reducer/Use_Reducer.js
@@ -1,11 +1,45 @@
-import React from 'react'
+'use client'
+import React, { useRef, useState } from 'react'
 import Sample from './smaple/Sample'
+import Calculator from './calculatur/Calculator'
 import styles from './usereducer.module.css'
+import Link from 'next/link'
 
+const items = [
+  {
+    id: 1,
+    title: 'Increase',
+    page: <Sample />
+  },
+  {
+    id: 2,
+    title: 'Calculator',
+    page: <Calculator />
+  }
+
+]
 function Use_Reducer() {
+  const [item,setItem]=useState(1)
   return (
     <div className={styles.reducer} >
-        <Sample/>
+      
+      <div className={styles.menu}>
+        {
+          items.map(item => {
+            return <span 
+            onClick={(e)=>{
+              setItem(e.target.id.slice(1,2))
+            }} id={'#'+item.id} key={item.id}>
+              {item.title}
+            </span>
+          })
+        }
+      </div>
+      <div className={styles.show}>
+        {
+          items[item-1].page
+        }
+      </div>
     </div>
   )
 }
---
+++ src/components/hooks/use_reducer/calculatur/Calculator.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './calculator.module.css'
+
+function Calculator() {
+  return (
+    <div className={styles.calculator}>Calculator</div>
+  )
+}
+
+export default Calculator
