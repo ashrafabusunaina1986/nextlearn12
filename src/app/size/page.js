78181712c8b365dc388b1ab557ae1967b5f48a40
ashraf <ashrafabusunaina1986>
2023-08-31 19:10:11 +0300

    first commit

---
+++ src/app/size/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './page.module.css'
+
+function Size() {
+  return (
+    <div className={styles.size}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam suscipit eligendi, dicta tempore quaerat officiis magnam dolorem aspernatur recusandae laboriosam?
+    </div>
+  )
+}
+
+export default Size
