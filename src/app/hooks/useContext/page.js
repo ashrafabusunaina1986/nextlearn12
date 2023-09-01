687f63bc6c31331eacb74457becccaeaf2e11902
ashraf <ashrafabusunaina1986>
2023-09-01 10:32:46 +0300

    first commit

---
+++ src/app/hooks/useContext/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './page.module.css'
+
+function UseContext() {
+  return (
+    <div className={styles.container}>UseContext</div>
+  )
+}
+
+export default UseContext
